a97180ee2000714b769d8624374a5a71f869fa54
Grace Li <grace.li@anl.gov>
2025-06-12 21:48:57 +0000

    updates to json, made test json to play around with

--- omb/run_auto_single.sh
+++ omb/run_auto_single.sh
@@ -14,8 +14,7 @@ mpiexec \
   -genv LD_LIBRARY_PATH $HOME/rccl/build/lib:/soft/compilers/rocm/rocm-6.3.2/lib:/soft/compilers/rocm/rocm-6.3.2/lib64:$HOME/grace_mpich/build/install/lib:$LD_LIBRARY_PATH \
   -genv MPIR_CVAR_ALLREDUCE_CCL auto \
   -genv MPIR_CVAR_DEVICE_COLLECTIVES none \
-  -genv MPIR_CVAR_COLL_SELECTION_TUNING_JSON_FILE ./tuning.json \
-  -genv MPIR_CVAR_COLL_SELECTION_VERBOSE 2 \
-  -genv MPIR_CVAR_ACCELERATOR rocm \
+  -genv MPIR_CVAR_COLL_SELECTION_TUNING_JSON_FILE tuning.json \
+  -genv MPIR_CVAR_COLL_SELECTION_VERBOSE 5 \
   -genv MPIR_CVAR_COLL_SELECTION_TUNING_JSON_VERBOSE 1 \
-  "$BIN" -m 0:1048576 -i 10000 2>&1 | tee auto_single.log
+  "$BIN" -m 0:1048576 -i 10000
